6ad44806abfc16bf2091b52404871edffe9b4167
Alexandr Sysoev <sanchous.ok@hotmail.com>
2017-06-13 13:06:16 +0300

    build 0.2.0
    * Binary max-root Heap added
    * Readme update
    * Tests update

---
+++ lib/Heap/binary_heap/binary_heap_max.rb
@@ -0,0 +1,85 @@
+module Heap
+  module BinaryHeap
+    class MaxHeap
+      attr_reader :elements
+
+      def initialize(elements = [])
+        @elements = []
+        add(elements.pop) until elements.empty?
+      end
+
+      def add(element)
+        @elements.push element
+        swim_up(count)
+      end
+
+      def count
+        @elements.length
+      end
+
+      def extract_max
+        @elements[0]
+      end
+
+      def extract_max!
+        swap(1, count)
+        el = @elements.pop
+        swim_down(1)
+        el
+      end
+
+      def sort
+        el_temp = @elements.clone
+        result = []
+        result.push extract_max! while count > 0
+        @elements = el_temp
+        result
+      end
+
+      private
+
+      def swap(index1, index2)
+        temp = @elements[index1 - 1]
+        @elements[index1 - 1] = @elements[index2 - 1]
+        @elements[index2 - 1] = temp
+      end
+
+      def swim_up(index)
+        return if index == 1
+        parent_index = (index / 2).floor
+        return if @elements[parent_index - 1] >= @elements[index - 1]
+        swap(parent_index, index)
+        swim_up parent_index
+      end
+
+      def swim_down(index)
+        child1_index = 2 * index
+        child2_index = 2 * index + 1
+        return if @elements[child1_index - 1].nil? && @elements[child2_index - 1].nil?
+        if @elements[child2_index - 1].nil?
+          return if @elements[child1_index - 1] <= @elements[index - 1]
+          swap(index, child1_index)
+          swim_down(child1_index)
+        else
+          if @elements[child2_index - 1] <= @elements[index - 1] && @elements[child1_index - 1] <= @elements[index - 1]
+            return
+          elsif @elements[child2_index - 1] > @elements[index - 1] && @elements[child1_index - 1] > @elements[index - 1]
+            if @elements[child2_index - 1] > @elements[child1_index - 1]
+              swap(child2_index, index)
+              swim_down(child2_index)
+            else
+              swap(child1_index, index)
+              swim_down(child1_index)
+            end
+          elsif @elements[child2_index - 1] > @elements[index - 1]
+            swap(child2_index, index)
+            swim_down(child2_index)
+          elsif @elements[child1_index - 1] > @elements[index - 1]
+            swap(child1_index, index)
+            swim_down(child1_index)
+          end
+        end
+      end
+    end
+  end
+end
--- test/Heap_test.rb
+++ test/Heap_test.rb
@@ -10,14 +10,26 @@ class HeapTest < Minitest::Test
     assert_equal 4, b_heap.count
   end
 
-  def test_binary_min_heap_add_and_min
+  def test_binary_max_heap_count
+    b_heap = Heap::BinaryHeap::MaxHeap.new([3, 2, 5, 7])
+    assert_equal 4, b_heap.count
+  end
+
+  def test_bin_min_heap_add_and_min
     b_heap = Heap::BinaryHeap::MinHeap.new([3, 2, 5, 7])
     b_heap.add 1
     assert_equal 5, b_heap.count
     assert_equal 1, b_heap.extract_min
   end
 
-  def test_binary_min_heap_extract_min
+  def test_bin_max_heap_add_and_max
+    b_heap = Heap::BinaryHeap::MaxHeap.new([3, 2, 5, 7])
+    b_heap.add 1
+    assert_equal 5, b_heap.count
+    assert_equal 7, b_heap.extract_max
+  end
+
+  def test_bin_min_heap_extract_min
     b_heap = Heap::BinaryHeap::MinHeap.new([3, 2, 5, 7])
     b_min = b_heap.extract_min!
     assert_equal 2, b_min
@@ -25,10 +37,25 @@ class HeapTest < Minitest::Test
     assert_equal 3, b_heap.extract_min
   end
 
+  def test_bin_max_heap_extract_max
+    b_heap = Heap::BinaryHeap::MaxHeap.new([3, 2, 5, 7])
+    b_max = b_heap.extract_max!
+    assert_equal 7, b_max
+    assert_equal 3, b_heap.count
+    assert_equal 5, b_heap.extract_max
+  end
+
   def test_binary_min_heap_sort
     b_heap = Heap::BinaryHeap::MinHeap.new([3, 2, 5, 7, -1, 10, 2, 5, -8])
     sorted = b_heap.sort
     assert_equal [-8, -1, 2, 2, 3, 5, 5, 7, 10], sorted
+    assert_equal 9, b_heap.count
+  end
+
+  def test_binary_max_heap_sort
+    b_heap = Heap::BinaryHeap::MaxHeap.new([3, 2, 5, 7, -1, 10, 2, 5, -8])
+    sorted = b_heap.sort
+    assert_equal [10, 7, 5, 5, 3, 2, 2, -1, -8], sorted
     assert_equal 9, b_heap.count
   end
 end
